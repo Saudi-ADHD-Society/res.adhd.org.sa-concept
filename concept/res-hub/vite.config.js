4b13668d7c9f6c8cd7a074f13d9f574cd1a8da83
Jeremy Varnham <jeremy@adhd.org.sa>
2025-12-18 13:22:53 +0300

    Workflow Settings

--- concept/res-hub/vite.config.js
+++ concept/res-hub/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages will serve this app at:
+  // https://saudi-adhd-society.github.io/res.adhd.org.sa-concept
+  // so we set the base path to the repository name.
+  base: '/res.adhd.org.sa-concept/',
 })
